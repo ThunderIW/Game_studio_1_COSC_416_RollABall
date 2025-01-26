5aab1a4454b99aed593e7bf7aacc47bfe96f769c
ThunderIW <43197270+ThunderIW@users.noreply.github.com>
2025-01-26 12:09:13 -0800

    Update InputManager.cs

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -5,22 +5,44 @@ public class InputManager : MonoBehaviour
 {
     public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     public UnityEvent OnJump = new UnityEvent();
+
     void Update()
     {
         Vector2 inputVector = Vector2.zero;
 
-        // Process input for W, A, S, D keys
-        if (Input.GetKey(KeyCode.W)) inputVector += Vector2.up;
-        if (Input.GetKey(KeyCode.S)) inputVector += Vector2.down;
-        if (Input.GetKey(KeyCode.A)) inputVector += Vector2.left;
-        if (Input.GetKey(KeyCode.D)) inputVector += Vector2.right;
-        
+        // Process input for W, A, S, D keys and log the pressed keys
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += Vector2.up;
+            Debug.Log("W key pressed");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector += Vector2.down;
+            Debug.Log("S key pressed");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+            Debug.Log("A key pressed");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector += Vector2.right;
+            Debug.Log("D key pressed");
+        }
+
         // Invoke the event with the input direction
-        OnMove?.Invoke(inputVector);
+        if (inputVector != Vector2.zero) // Log only when there is movement
+        {
+            
+            OnMove?.Invoke(inputVector);
+        }
 
-
-        if (Input.GetKey(KeyCode.Space)) {
-            Debug.Log("Jump Pressed");
+        // Process and log input for Space key (jump)
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Space key pressed");
             OnJump?.Invoke();
         }
     }
